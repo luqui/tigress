360ee33393b59238b81541e76382ccc4a6138108
Luke Palmer <luqui@clozure.com>
2012-11-20 16:30:40 -0700

    Apparently correct unify function.

--- Tigress.hs
+++ Tigress.hs
@@ -1,14 +1,19 @@
+{-# LANGUAGE TupleSections #-}
+
 module Tigress where
 
 import qualified Data.Map as Map
 import qualified Data.Set as Set
+import Control.Applicative
 import Control.Monad.Logic
 import Control.Monad.State
+import Control.Arrow
 
 class Concrete c where
 
 type VarName = String
 type PredName = String
+type DefID = String
 
 data Prop = App PredName [Object]
 
@@ -16,74 +21,73 @@ data Rule = [Prop] :=> Prop
 
 data Object
     = Var VarName
-
-data Definition c = Definition {
-    defnDom :: Set.Set VarName,
-    defnCod :: Set.Set VarName,
-    defnMap :: Map.Map VarName c -> Map.Map VarName c
-}
-
-data Binding c = Binding Rule (Definition c)
+    | Cons DefID [Object]
+    deriving (Show)
 
 class FreeVars a where freeVars :: a -> Set.Set VarName
 instance (FreeVars a) => FreeVars [a] where freeVars = Set.unions . map freeVars
-instance FreeVars Object where freeVars (Var v) = Set.singleton v
+instance (FreeVars a) => FreeVars (Map.Map k a) where freeVars = freeVars . Map.elems
+instance FreeVars Object where 
+    freeVars (Var v) = Set.singleton v
+    freeVars (Cons _ args) = freeVars args
 instance FreeVars Prop where freeVars (App pred objs) = freeVars objs
 instance FreeVars Rule where freeVars (hyps :=> con) = freeVars hyps `Set.union` freeVars con
 
-checkBinding :: Binding c -> Bool
-checkBinding (Binding (hyps :=> con) defn) = (defnDom defn `Set.isSubsetOf` freeVars hyps) && (freeVars con `Set.isSubsetOf` defnCod defn)
+class Subst a where subst :: Map.Map VarName Object -> a -> a
+instance (Subst a) => Subst [a] where subst s = map (subst s)
+instance (Subst a) => Subst (Map.Map k a) where subst s = Map.map (subst s)
+instance Subst Object where
+    subst s (Var v) = maybe (Var v) id (Map.lookup v s)
+    subst s (Cons did args) = Cons did (subst s args)
+instance Subst Prop where
+    subst s (App pred args) = App pred (subst s args)
+instance Subst Rule where
+    subst s (hyps :=> con) = subst s hyps :=> subst s con
 
-data DB c = DB {
-    dbBindings :: Map.Map PredName [Binding c]
+data SolverState = SolverState {
+    ssFresh :: Integer,
+    ssSubst :: Map.Map VarName Object
 }
 
-type Solver = StateT Integer Logic
+type Solver = StateT SolverState Logic
 
 alloc :: Solver VarName
 alloc = do
-    x <- get
-    put $! x+1
+    s <- get
+    let x = ssFresh s
+    put $ s { ssFresh = 1 + x }
     return ("~" ++ show x)
 
-try :: (MonadPlus m) => Maybe a -> m a
-try = maybe mzero return
+addSub :: VarName -> Object -> Solver ()
+addSub v o
+    | v `Set.member` freeVars o = mzero
+    | otherwise = do
+        s <- get
+        let s' = Map.insert v o (subst (Map.singleton v o) (ssSubst s))
+        put $ s { ssSubst = s' }
+      
+normalize :: (Subst a) => a -> Solver a
+normalize x = do
+    s <- gets ssSubst
+    return $ subst s x
 
+zip' :: (Functor m, MonadPlus m) => [a] -> [b] -> m [(a,b)]
+zip' [] [] = return []
+zip' (x:xs) (y:ys) = ((x,y):) <$> zip' xs ys
+zip' _ _ = mzero
+ 
+unify :: Object -> Object -> Solver ()
+unify x y = do
+    x' <- normalize x
+    y' <- normalize y
+    case (x', y') of
+        (Var a, Var b)           | a == b  -> return ()
+        (Var a, obj)                       -> addSub a obj
+        (Cons h as, Cons h' as') | h == h' -> mapM_ (uncurry unify) =<< zip' as as'
+        _                                  -> fail "No unification"
 
-
-
-
--- bar x y   { x=..; y=.. }
--- bar A Y => foo A z  { z=.. }
-
--- foo P Q
---  bar P Y
---   
-
--- list a = nil | cons a (list a)   ;; defines nil, cons, case
--- xs == ys = ... Nil ... Cons ...
--- reverse xs = ... Nil ... Cons ...
--- isPalin xs = xs == reverse xs
--- 
--- (list nil cons case)  ;; relate nil, cons, case as an instance of a list abstraction
---                       ;; (perhaps (list :nil nil :cons cons :case case))
--- 
--- (=> (list nil cons case) (eq ==))  ;; for any list, there is an instance of an == abstraction
--- 
--- (=> (list nil cons case) (reverse reverse)) ;; for any list, there is an instance of a reverse abstraction
--- 
--- (=> (eq ==) (reverse reverse) (isPalin isPalin)) ;; for any eq and reverse instances, there is an isPalin instance
--- 
--- But how do we know that eq and reverse are related at the same point.  There is no way for the solver to
--- connect those, so it might pick an eq on int and a reverse on Map, and end up with nonsense.
--- 
--- Of course, if types are in the game...
--- 
--- (forall (A) (list listA A nil cons case))   ;; list is a polymorphic instance of a list abstraction
--- 
--- (=> (list listA A nil cons case) (eq a ==) (eq list ==*)) ;; if list is an instance of list, and == is an instance of eq, then list is also an instance of eq
--- 
--- (=> (list listA A nil cons case) (reverse listA reverse))
--- 
--- (=> (eq A ==) (reverse A reverse) (isPalin A isPalin))
-
+runSolver :: Solver a -> [(Map.Map VarName Object, a)]
+runSolver solver = map (first ssSubst . swap) . observeAll $ runStateT solver state0
+    where
+    state0 = SolverState { ssFresh = 0, ssSubst = Map.empty }
+    swap (x,y) = (y,x)
